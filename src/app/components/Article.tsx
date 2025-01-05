f55174ebd311ee76ffcc0cc5c0037631de866ba0
t-takahashi <tttol.t4k.b1z@gmail.com>
2025-01-05 15:49:38 +0900

    fix

--- src/app/components/Article.tsx
+++ src/app/components/Article.tsx
@@ -3,14 +3,16 @@ import { Schema } from "../../../amplify/data/resource";
 import noImage from "./../img/noimage.png";
 
 export default function Article({ article }: { article: Schema["Article"]["type"] }) {
-  console.log(noImage.blurDataURL);
   return (
     <div className="flex border-b-[1px] border-b-slate-400">
       <div className="w-3/4 p-1">
         <div>
           <a href={article.link} target="_blank">
-            <div className="font-bold">{article.title}</div>
-            <div className="text-slate-500">{article.siteName}</div>
+            <div className={article.isRead ? "" : "font-bold"}>
+              {!article.isRead && <div className="w-2 h-2 inline-block mr-2 bg-blue-500 rounded-full"></div>}
+              {article.title}
+            </div>
+            <div className="text-slate-500">{article.siteName} / {new Date(article.publishedAt).toLocaleString()}</div>
           </a>
         </div>
       </div>
