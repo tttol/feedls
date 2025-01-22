fe294aeb20ffb04ebb0e991dc99a168cf2b29547
t-takahashi <tttol.t4k.b1z@gmail.com>
2025-01-23 06:16:44 +0900

    Revert "move generateClient logic to handler.ts"
    This reverts commit ce8341b408d4a2e133a9407499ee8e9fbb25bdaf.

--- amplify/functions/fetch-rss/handler.ts
+++ amplify/functions/fetch-rss/handler.ts
@@ -1,13 +1,14 @@
-import { Amplify } from "aws-amplify";
-import { generateClient } from "aws-amplify/data";
+// import outputs from "@/../../amplify_outputs.json";
+// import { Amplify } from "aws-amplify";
+// import { generateClient } from "aws-amplify/data";
 import { Handler } from 'aws-cdk-lib/aws-lambda';
+import { getLastFetchedTime, putArticle, wirteLastFetchedTime } from "../../../src/app/lib/ddb";
 import { RssObj } from '../../../src/app/lib/types';
 import { fetchRss, parseXml } from "../../../src/app/lib/utils";
-import { Schema } from '../../data/resource';
-import outputs from "./../../../amplify_outputs.json";
+// import { Schema } from '../../data/resource';
 
-Amplify.configure(outputs);
-const client = generateClient<Schema>();
+// Amplify.configure(outputs);
+// const client = generateClient<Schema>();
 
 export const handler: Handler = async (event: any, context: any) => {
   console.debug("event:", event, "context", context);
@@ -15,6 +16,7 @@ export const handler: Handler = async (event: any, context: any) => {
   const currentTime: Date = new Date();
   const lastFetchedTime: Date = await getLastFetchedTime();
   console.debug("lastFetchedTime:", lastFetchedTime);
+
 
   let totalCount = 0;
   for (const u of urls) {
@@ -29,51 +31,3 @@ export const handler: Handler = async (event: any, context: any) => {
   await wirteLastFetchedTime();
   return `ok. ${totalCount} articles were inserted.`;
 };
-
-
-export const getLastFetchedTime: () => Promise<Date> = async () => {
-  const { errors, data } = await client.models.FetchedHistory.get({ id: "1" });
-
-  if (errors !== undefined || data == null) {
-    console.error("error:", errors, "data:", data);
-    throw new Error("Failed to get FetchedHistory.")
-  }
-  console.debug("FetchedHistory:", data);
-  return new Date(data.fetchedAt);
-}
-
-export const wirteLastFetchedTime = async () => {
-  await client.models.FetchedHistory.update({
-    id: "1",
-    fetchedAt: new Date().toISOString(),
-  });
-}
-
-export const putArticle = async(rssObj: RssObj, currentTime: Date, lastFetchedTime: Date) => {
-  const siteName = rssObj.rss.channel.title;
-  let insertedArticleCnt = 0;
-  for (const item of rssObj.rss.channel.item) {
-    if (new Date(item.pubDate) <= lastFetchedTime) continue;
-
-    try {
-      await client.models.Article.create({
-        siteName: siteName,
-        title: item.title,
-        link: item.link,
-        enclosureUrl: item.enclosure["@_url"],
-        enclosureType: item.enclosure["@_type"],
-        aiSummary: "",
-        isRead: false,
-        isDeleted: false,
-        publishedAt: item.pubDate,
-        fetchedAt: currentTime.toISOString(),
-      });
-      insertedArticleCnt++;
-    } catch (e) {
-      console.error("Failed to put article.", item, e);
-      continue;
-    }
-  }
-
-  return insertedArticleCnt;
-}
